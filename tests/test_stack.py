7e5ad914611363a0650099056c5c3e05a508a65d
Hiromu Shioya <kwappa.856@gmail.com>
2024-06-11 10:19:02 +0900

    :repeat: Add setUp

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -3,11 +3,12 @@ from tdd_stack.stack import Stack
 
 
 class TestTddStack(unittest.TestCase):
+    def setUp(self):
+        self.stack = Stack()
+
     def test_create(self):
-        stack = Stack()
-        self.assertTrue(stack.is_empty())
+        self.assertTrue(self.stack.is_empty())
 
     def test_push_and_top(self):
-        stack = Stack()
-        stack.push(1)
-        self.assertEqual(1, stack.top())
+        self.stack.push(1)
+        self.assertEqual(1, self.stack.top())
