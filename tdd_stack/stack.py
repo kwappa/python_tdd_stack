c3fd2b8a65d28804398efc08ac07984bc7ab5ff1
Hiromu Shioya <kwappa.856@gmail.com>
2024-06-11 10:26:36 +0900

    :repeat: Implement to increase size when push called

--- tdd_stack/stack.py
+++ tdd_stack/stack.py
@@ -1,15 +1,17 @@
 class Stack:
     def __init__(self):
         self._value = 0
+        self._size = 0
 
     def is_empty(self):
         return True
 
     def push(self, value):
         self._value = value
+        self._size += 1
 
     def top(self):
         return self._value
 
     def size(self):
-        return 1
+        return self._size
